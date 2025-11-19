f2dfc87f7bc15c45e2bb3ebeeafd2f614e3a7ab3
Sümeyra Nur Kayalar <149170501+SumeyraNurKayalar@users.noreply.github.com>
2025-11-19 23:20:02 +0300

    Trying to fix the siamese model part

--- layers.py
+++ layers.py
@@ -1,20 +1,20 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Layer
 
-class L1Dist(Layer):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        
-    def call(self, inputs):
-        # Inputs bir tuple olarak geliyor: (input_embedding, validation_embedding)
-        input_embedding, validation_embedding = inputs
-        
-        # Tensor'ları doğrudan al
-        return tf.math.abs(input_embedding - validation_embedding)
+def make_siamese_model():
+    input_image = tf.keras.layers.Input(name='input_img', shape=(100, 100, 3))
+    validation_image = tf.keras.layers.Input(name='validation_img', shape=(100, 100, 3))
+
+    embedding_model = make_embedding()
+
+    inp_embedding = embedding_model(input_image)
+    val_embedding = embedding_model(validation_image)
     
-    def compute_output_shape(self, input_shape):
-        # Input shape bir tuple: (shape1, shape2)
-        return input_shape[0]
+    distances = tf.keras.layers.Lambda(
+        lambda x: tf.abs(x[0] - x[1]), 
+        name='l1_distance'
+    )([inp_embedding, val_embedding])
+
+    classifier = tf.keras.layers.Dense(1, activation='sigmoid')(distances)
     
-    def get_config(self):
-        return super().get_config()
+    return tf.keras.models.Model(inputs=[input_image, validation_image], outputs=classifier, name='SiameseNetwork')
